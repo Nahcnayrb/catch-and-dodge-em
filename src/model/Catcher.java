fc71b97a29043e72fd7aa28279d6ed63bcd7cf3f
ngynjsn <nguyen.j1305@gmail.com>
2021-01-09 13:27:52 -0800

    catcher and catchemgame changed!

--- src/model/Catcher.java
+++ src/model/Catcher.java
@@ -1,11 +1,14 @@
 package model;
 
+import java.awt.*;
+
 public class Catcher {
     private int x;
     private int y;
     private static final int SPEED = 2;
     private static final int SIZE_X = 5;
-    private static final int SIZE_Y = 5;
+    private static final int SIZE_Y = 10;
+    private static final Color COLOR = new Color(255,255,255);
 
     public Catcher(){
         this.x = CatchEmGame.WIDTH/2;
@@ -18,5 +21,12 @@ public class Catcher {
 
     public int getY() {
         return y;
+    }
+
+    public void draw(Graphics g) {
+        Color savedColour = g.getColor();
+        g.setColor(COLOR);
+        g.fillRect(getX() - SIZE_X/2, getY() - SIZE_Y/2, SIZE_X, SIZE_Y);
+        g.setColor(savedColour);
     }
 }
