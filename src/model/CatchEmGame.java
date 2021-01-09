12e5ce0904a2a45c3347ddc1c7f91717cd1f3975
ngynjsn <nguyen.j1305@gmail.com>
2021-01-09 13:20:54 -0800

    catcher and catchemgame changed

---
+++ src/model/CatchEmGame.java
@@ -0,0 +1,6 @@
+package model;
+
+public class CatchEmGame {
+    public static final int WIDTH = 400;
+    public static final int HEIGHT = 400;
+}
--- src/model/Catcher.java
+++ src/model/Catcher.java
@@ -1,4 +1,22 @@
 package model;
 
 public class Catcher {
+    private int x;
+    private int y;
+    private static final int SPEED = 2;
+    private static final int SIZE_X = 5;
+    private static final int SIZE_Y = 5;
+
+    public Catcher(){
+        this.x = CatchEmGame.WIDTH/2;
+        this.y = CatchEmGame.HEIGHT/2;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
