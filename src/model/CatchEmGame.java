0428c2bbfba19c36ef5b8dda2bff8ee848003a35
ngynjsn <nguyen.j1305@gmail.com>
2021-01-09 13:48:37 -0800

    game logic added in catcheemgame

--- src/model/CatchEmGame.java
+++ src/model/CatchEmGame.java
@@ -1,8 +1,42 @@
 package model;
 
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 public class CatchEmGame {
     public static final int WIDTH = 800;
     public static final int HEIGHT = 800;
+    private Catcher catcher;
 
+    public void moveCatcher(String dir) {
+        if (dir.equals("left")) {
+            catcher.moveLeft();
+        } else if (dir.equals("right")) {
+            catcher.moveRight();
+        } else if (dir.equals("up")) {
+            catcher.moveUp();
+        } else if (dir.equals("down")) {
+            catcher.moveUp();
+        }
+    }
 
+    public class UserKeyAdapter extends KeyAdapter {
+
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    moveCatcher("left");
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    moveCatcher("right");
+                    break;
+                case KeyEvent.VK_UP:
+                    moveCatcher("up");
+                    break;
+                case KeyEvent.VK_DOWN:
+                    moveCatcher("down");
+                    break;
+            }
+        }
+    }
 }
