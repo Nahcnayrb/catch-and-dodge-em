dbe87411cd572307634016746b3515c8d9360b7a
Nahcnayrb <69608751+Nahcnayrb@users.noreply.github.com>
2021-01-09 13:54:11 -0800

    "Finished implementing ball class"

--- src/model/Ball.java
+++ src/model/Ball.java
@@ -12,14 +12,52 @@ public class Ball {
     private int x;
     private int y;
     private int verticalDirection;
-    private int speed;
+    private int horizontalSpeed;
+    private int verticalSpeed;
 
 
     public Ball() {
         x = 0;
         y = (int) ((Math.random() * (MAX_Y - MIN_Y)) + MIN_Y);
-        speed = (int) ((Math.random() * (MAX_SPEED - MIN_SPEED)) + MIN_SPEED);
+        horizontalSpeed = (int) ((Math.random() * (MAX_SPEED - MIN_SPEED)) + MIN_SPEED);
+        verticalSpeed = (int) ((Math.random() * (MAX_SPEED - MIN_SPEED)) + MIN_SPEED);
         verticalDirection = NEUTRAL;
+    }
+
+    private int getNewVerticalDirection() {
+        return (int) ((Math.random() * 3));
+
+
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getHorizontalSpeed() {
+        return horizontalSpeed;
+    }
+
+    public int getVerticalSpeed() {
+        return verticalSpeed;
+    }
+
+    public int getVerticalDirection() {
+        return verticalDirection;
+    }
+
+    public void tick() {
+        if (getVerticalDirection() == UP) {
+            y = y - verticalSpeed;
+        } else if (getVerticalDirection() == DOWN) {
+            y = y + verticalSpeed;
+        }
+        x = x + horizontalSpeed;
+        verticalDirection = getNewVerticalDirection();
     }
 
 }
