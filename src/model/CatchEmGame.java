abc99f4452ee095a8cd3be5d59cc19f0d0976e75
ngynjsn <nguyen.j1305@gmail.com>
2021-01-09 13:22:00 -0800

    catcher and catchemgame changed

--- src/model/CatchEmGame.java
+++ src/model/CatchEmGame.java
@@ -1,6 +1,6 @@
 package model;
 
 public class CatchEmGame {
-    public static final int WIDTH = 400;
-    public static final int HEIGHT = 400;
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 800;
 }
