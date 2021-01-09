b49978655c076431ad6aad9dd3c4b06aa9c75d7e
ngynjsn <nguyen.j1305@gmail.com>
2021-01-09 15:15:37 -0800

    added getheight and getwidth

--- src/model/Ball.java
+++ src/model/Ball.java
@@ -53,6 +53,14 @@ public class Ball {
         return verticalDirection;
     }
 
+    public int getWidth() {
+        return WIDTH;
+    }
+
+    public int getHeight() {
+        return HEIGHT;
+    }
+
     public void bounceDown() {
         verticalDirection = DOWN;
     }
